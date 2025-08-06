532ec96ba72219e71763432f107d3c527f93abfc
Amit Arefin <amitarefin2@gmail.com>
2025-08-05 22:09:26 -0400

    Added a couple of statistics

--- part1/project_unicafe/src/App.jsx
+++ part1/project_unicafe/src/App.jsx
@@ -22,6 +22,10 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>Total: {good + neutral + bad}</p>
+      <p>Average: {(good + neutral + bad)/3}</p>
+      <p>Positive: {(good/(good + neutral + bad))*100}%</p>
+
     </div>
   )
 }
